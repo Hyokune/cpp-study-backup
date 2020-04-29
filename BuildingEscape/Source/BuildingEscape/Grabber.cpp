90087d17bd6119f75324f605a11cef4a5827125c
Lance <19813076+Hyokune@users.noreply.github.com>
2020-04-29 22:05:22 +1200

    Added automatic door close + Added Grabber component (#14)

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,44 @@
+// Copyright Hyokune 2020
+
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get players viewpoint
+
+  FVector PlayerViewPointLocation;
+  FRotator PlayerViewPointRotation;
+  GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+  UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+  // Ray-cast out to a certain distance (Reach)
+
+  // See what it hits
+}
+
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -23,10 +24,30 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+  void OpenDoor(const float& DeltaTime);
+  void CloseDoor(const float& DeltaTime);
 
 private:
-  float InitialYaw;
-  float CurrentYaw;
+  float InitialAngle;
+  float CurrentAngle;
+
   UPROPERTY(EditAnywhere)
-  float TargetYaw = 90.f;
+  float OpenAngle = 90.f;
+
+  float DoorLastOpened = 0.f;
+
+  UPROPERTY(EditAnywhere)
+  float DoorCloseDelay = 0.5f;
+
+  UPROPERTY(EditAnywhere)
+  float DoorOpenSpeed = 1.6f;
+
+  UPROPERTY(EditAnywhere)
+  float DoorCloseSpeed = 2.f;
+
+  UPROPERTY(EditAnywhere)
+  ATriggerVolume* PressurePlate;
+
+  UPROPERTY(EditAnywhere)
+  AActor* ActorThatOpens;
 };
