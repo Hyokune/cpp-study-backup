5f42fa7d15a5ee8bd631d71049da50e0822f605f
Hyokune <19813076+Hyokune@users.noreply.github.com>
2020-05-01 22:54:09 +1200

    Added Grab/Release action to Pawn

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,8 +1,9 @@
 // Copyright Hyokune 2020
 
-#include "GameFramework/PlayerController.h"
-#include "Engine/World.h"
 #include "Grabber.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 #define OUT
 
@@ -12,17 +13,15 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+  FindPhysicsHandle();
+  SetupInputComponent();
 }
 
 
@@ -31,14 +30,102 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Get players viewpoint
+  if (PhysicsHandle->GrabbedComponent)
+  {
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+    // Ray-cast out to a certain distance (Reach)
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+    PhysicsHandle->SetTargetLocation(LineTraceEnd);
+  }
+}
+
+void UGrabber::FindPhysicsHandle()
+{
+  PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+  if (PhysicsHandle)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("Physics Handle component found on %s"), *GetOwner()->GetName());
+  }
+  else
+  {
+    UE_LOG(LogTemp, Error, TEXT("No Physics Handle component found on %s!"), *GetOwner()->GetName());
+  }
+}
+
+void UGrabber::SetupInputComponent()
+{
+  InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+  if (InputComponent)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("Input component found on %s"), *GetOwner()->GetName());
+    InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+    InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+  }
+  else
+  {
+    UE_LOG(LogTemp, Error, TEXT("No Input component found on %s!"), *GetOwner()->GetName());
+  }
+}
+
+void UGrabber::Grab()
+{
+  UE_LOG(LogTemp, Warning, TEXT("Grabber pressed"));
 
   FVector PlayerViewPointLocation;
   FRotator PlayerViewPointRotation;
-  GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
-  UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
-  // Ray-cast out to a certain distance (Reach)
 
-  // See what it hits
+  GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+  // Ray-cast out to a certain distance (Reach)
+  FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+  FHitResult HitResult = GetFirstPhysicsBodyInReach();
+
+  if (HitResult.GetActor())
+  {
+    UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+
+    PhysicsHandle->GrabComponentAtLocation(ComponentToGrab, NAME_None, LineTraceEnd);
+  }
 }
 
+void UGrabber::Release()
+{
+  UE_LOG(LogTemp, Warning, TEXT("Grabber released"));
+  
+  // Remove/Release PhysicsHandle
+  PhysicsHandle->ReleaseComponent();
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
+  // Get players viewpoint
+  FVector PlayerViewPointLocation;
+  FRotator PlayerViewPointRotation;
+
+  GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+  // Ray-cast out to a certain distance (Reach)
+  FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+  // DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(0, 255, 0), false, 0.f, 0, 5.f);
+
+  FHitResult Hit;
+  FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+  // See what it hits
+  GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParams);
+  AActor* ActorHit = Hit.GetActor();
+
+  if (ActorHit)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("Line trace has hit: %s"), *(ActorHit->GetName()));
+  }
+
+  return Hit;
+}
--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,9 +1,9 @@
 // Copyright Hyokune 2020
 
+#include "OpenDoor.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
-#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
